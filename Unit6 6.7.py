6a4fb176d35be5bfe6ec60b6a5ec19764c74f748
김예원 <77113053+yeaone@users.noreply.github.com>
2021-01-11 12:35:52 +0900

    Add files via upload

---
+++ Unit6 6.7.py
@@ -0,0 +1,8 @@
+a=50
+b=100
+c=None
+
+print(a)
+print(b)
+print(c)
+
---
+++ Unit9 9.4.py
@@ -0,0 +1,6 @@
+s="""\'Python\' is a \"programming language\"
+tha lets you work quickly
+and
+integrate systems more effectively."""
+
+print(s)
